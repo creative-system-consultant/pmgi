ea9c24a634d7088a7cb77b5b0ff0b4e12ab1936f
MnamesJeff <iskandar.rahman@iium.edu.my>
2024-09-06 15:29:30 +0800

    Session Info Linkage

--- app/Models/SessionInfo.php
+++ app/Models/SessionInfo.php
@@ -9,13 +9,18 @@ class SessionInfo extends Model
 {
     use HasFactory;
 
-    protected $table="PMGI_SESSION_INFO";
+    protected $table = "PMGI_SESSION_INFO";
     protected $guarded = [];
     public $timestamps = false;
 
     public function setting()
     {
         return $this->belongsTo(SettPymPmc::class, 'session_id', 'session_id');
+    }
+
+    public function meeting()
+    {
+        return $this->belongsTo(SettMeetingRoom::class, 'venue', 'id');
     }
 
     public function pydInfo()
