e56db9a826c60a828574670494253bf5f09fca78
Nazirul Fitri <nazirul@csc.net.my>
2024-09-07 23:55:29 +0800

    Up polling time for perakuan

--- resources/views/livewire/loading-perakuan.blade.php
+++ resources/views/livewire/loading-perakuan.blade.php
@@ -1,5 +1,5 @@
 <main>
-    <div class="px-4 pt-6 2xl:px-0" wire:poll.2s="checkRecord">
+    <div class="px-4 pt-6 2xl:px-0" wire:poll.5s="checkRecord">
         <div class="p-4 my-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 ">
             <div class="flex flex-col items-center justify-center">
                 <img class="h-96" src="{{ asset('image/animation/loading.gif') }}" alt="Loading">
