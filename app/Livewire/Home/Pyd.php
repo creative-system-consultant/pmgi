6cbd3a3aeb7f5ae2ee770e429769e9765edbdc52
Nazirul Fitri <nazirul@csc.net.my>
2024-10-09 22:05:48 +0800

    pyd home

--- app/Livewire/Home/Pyd.php
+++ app/Livewire/Home/Pyd.php
@@ -124,25 +124,33 @@ class Pyd extends Component
     public function render()
     {
         $officerData = DB::table('PMGI_SUMM_MTH_OFFICER as a')
-            ->leftJoin('PMGI_MNTR_SESSION as b', function ($join) {
-                $join->on('a.officer_id', '=', 'b.officer_id')
-                    ->whereRaw("TO_CHAR(a.report_date, 'MM-YYYY') = TO_CHAR(b.session_date_start, 'MM-YYYY')")
-                    ->orWhereRaw("TO_CHAR(a.report_date, 'MM-YYYY') = TO_CHAR(b.session_date_end, 'MM-YYYY')");
-            })
-            ->where('a.officer_id', auth()->user()->userid)
-            ->select(
-                'a.report_date', 'a.officer_id', 'b.pmgi_result', 'b.pmgi_level',
-                'a.bil_selia', 'a.bil_dapat_kutip',
-                'a.rm_patut_kutip', 'a.rm_dapat_kutip', 'a.rm_dapat_kutip_pts',
-                'a.bil_lawat', 'a.bil_lawat_pts'
-            )
-            ->orderBy('a.report_date', 'asc')  //need to change to desc later.. now use asc since only earlier data has result
-            ->take(8)
-            ->get();
+    ->leftJoin('PMGI_MNTR_SESSION as b', function ($join) {
+        $join->on('a.officer_id', '=', 'b.officer_id')
+            ->whereRaw("TO_CHAR(a.report_date, 'MM-YYYY') = TO_CHAR(b.session_date_start, 'MM-YYYY')")
+            ->orWhereRaw("TO_CHAR(a.report_date, 'MM-YYYY') = TO_CHAR(b.session_date_end, 'MM-YYYY')");
+    })
+    ->where('a.officer_id', auth()->user()->userid)
+    ->select(
+        'a.report_date',
+        'a.officer_id',
+        'b.pmgi_result',
+        'b.pmgi_level',
+        'a.bil_selia',
+        'a.bil_dapat_kutip',
+        'a.rm_patut_kutip',
+        'a.rm_dapat_kutip',
+        'a.rm_dapat_kutip_pts',
+        'a.bil_lawat',
+        'a.bil_lawat_pts'
+    )
+    ->orderBy('a.report_date', 'desc')
+    ->take(8) // Equivalent to ROWNUM <= 8 in Oracle
+    ->get();
+
 
         // Format the report_date field
         $officerData->transform(function ($item) {
-            $item->report_date = Carbon::parse($item->report_date)->format('M-y');
+            $item->report_date = Carbon::parse($item->report_date)->translatedFormat('M-y');
             return $item;
         });
 
