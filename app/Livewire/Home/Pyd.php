635f2db637ea5495f4e26de60865119e44280d27
Nazirul Fitri <nazirul@csc.net.my>
2024-10-28 12:36:32 +0800

    dashboard PYD

--- app/Livewire/Home/Pyd.php
+++ app/Livewire/Home/Pyd.php
@@ -2,24 +2,53 @@
 
 namespace App\Livewire\Home;
 
+use App\Models\User;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Pyd extends Component
 {
+    public $userId;
+    public $user;
+    public $data;
     public $pmgiLevels;
     public $pmgiResults;
     public $penjadualanSemula;
     public $mia;
     public $pembiayaan;
+    public $username;
+    public $staffno;
+    public $jawatan;
+    public $stateName;
+    public $branchName;
+    public $tarikhLantikan;
+    public $tempohBerkhidmat;
 
     public function mount()
     {
-        $this->penjadualanSemula = DB::table('RESCHEDULE_INFO2 as rs')
-                        ->join('account_master as m', 'rs.accountno', '=', 'm.account_no')
-                        ->where('activestatus', '>=', 0)
-                        ->selectRaw('
+        if ($this->userId) {
+            $this->user = $this->userId;
+        } else {
+            $this->user = auth()->user()->userid;
+        }
+
+        $this->data = User::find($this->user);
+        $this->username = $this->data->username;
+        $this->staffno = $this->data->staffNo();
+        $this->jawatan = $this->data->bankOfficer->hrData->jawatan;
+        $this->stateName = $this->data->stateName();
+        $this->branchName = $this->data->branchName();
+        $this->tarikhLantikan = Carbon::parse($this->data->bankOfficer->hrData->tarikh_lantikan)->translatedFormat('d F Y');
+        $tempoh = $this->data->bankOfficer->hrData->tempoh_penempatan_semasa;
+        $this->tempohBerkhidmat = str_replace(['Y', 'M', 'D'], [' Tahun ', ' Bulan ', ' Hari'], $tempoh);
+
+        $this->penjadualanSemula = Cache::remember('penjadualan_semula', 480, function () {
+            return DB::table('RESCHEDULE_INFO2 as rs')
+                ->join('account_master as m', 'rs.accountno', '=', 'm.account_no')
+                ->where('activestatus', '>=', 0)
+                ->selectRaw('
                             count(*) as terima,
                             sum(case when nvl(branchapproval,0) = 1 then 1 else 0 end) as lulus,
                             sum(case when nvl(branchapproval,0) = 9 then 1 else 0 end) as tolak,
@@ -32,64 +61,69 @@ class Pyd extends Component
                             sum(case when nvl(branchapproval,0) in (0,2) then apprvlimit else 0 end) as jumbaki,
                             sum(case when nvl(branchapproval,0) = 8 then apprvlimit else 0 end) as jumbatal
                         ')
-                        ->first();
+                ->first();
+        });
 
-        $this->mia = DB::table('mia_appl_list as ma')
-                        ->join('account_master as m', 'ma.custid', '=', 'm.cust_id')
-                        ->selectRaw('
+        $this->mia = Cache::remember('mia', 480, function () {
+            return DB::table('mia_appl_list as ma')
+                ->join('account_master as m', 'ma.custid', '=', 'm.cust_id')
+                ->selectRaw('
                             count(*) as jumlah,
                             sum(case when ma.status = 1 then 1 else 0 end) as lulus,
                             sum(case when ma.status = 0 then 1 else 0 end) as proses,
                             sum(case when ma.status = -1 then 1 else 0 end) as tolak,
                             sum(case when ma.status = -2 then 1 else 0 end) as dikembalikan
                         ')
-                        ->first();
+                ->first();
+        });
 
         $currentDate = now()->format('d M Y');
 
-        $pembiayanData = DB::select("
-                            select nvl(uf_decode_prodcatg(product_catg), 0) as product_category,
-                                sum(bil) as bilakaun,
-                                sum(bilpeminjam) as bil_peminjam,
-                                sum(amt) as jumlah_pembiayaan
-                            from (
-                                SELECT count(*) AS bil,
-                                    count(distinct m.cust_id) as bilpeminjam,
-                                    nvl(sum(m.approved_limit), 0) AS amt,
-                                    nvl(SUBSTR(UF_GET_PRODUCT_CATG(m.PRODUCT_CODE, m.PRODUCT_SUB_CODE), 1, 3), 0) AS PRODUCT_CATG
-                                FROM account_master m
-                                left outer join DISBURSEMENT_REQUEST d
-                                on m.account_no = d.account_no
-                                where ((trunc(d.cheque_Date) <= to_Date(?, 'DD MON YYYY')  and disburse_mode = 'CH' AND d.disb_status not in ('X', 'R','S'))
-                                or (trunc(d.autodebit_Date) <= to_Date(?, 'DD MON YYYY') and disburse_mode = 'AD' AND d.disb_status not in ('X', 'R','S') and (length(d.DEBIT_BANKACCT) > 0)))
-                                and m.account_status <> 2
-                                group by SUBSTR(UF_GET_PRODUCT_CATG(m.PRODUCT_CODE, m.PRODUCT_SUB_CODE), 1, 3)
-                                union
-                                SELECT count(*) AS bil,
-                                    count(distinct M.Nokpbaru) as bilpeminjam,
-                                    sum(M.Jumlahpinjaman) AS amt,
-                                    M.Produk_Category AS PRODUCT_CATG
-                                FROM Migration_New M
-                                group by M.Produk_Category
-                            )
-                            group by nvl(uf_decode_prodcatg(product_catg), 0)
-                        ", [$currentDate, $currentDate]);
+        $this->pembiayaan = Cache::remember('pembiayaan_' . $currentDate, 480, function () use ($currentDate) {
+            $pembiayanData = DB::select("
+                                select nvl(uf_decode_prodcatg(product_catg), 0) as product_category,
+                                    sum(bil) as bilakaun,
+                                    sum(bilpeminjam) as bil_peminjam,
+                                    sum(amt) as jumlah_pembiayaan
+                                from (
+                                    SELECT count(*) AS bil,
+                                        count(distinct m.cust_id) as bilpeminjam,
+                                        nvl(sum(m.approved_limit), 0) AS amt,
+                                        nvl(SUBSTR(UF_GET_PRODUCT_CATG(m.PRODUCT_CODE, m.PRODUCT_SUB_CODE), 1, 3), 0) AS PRODUCT_CATG
+                                    FROM account_master m
+                                    left outer join DISBURSEMENT_REQUEST d
+                                    on m.account_no = d.account_no
+                                    where ((trunc(d.cheque_Date) <= to_Date(?, 'DD MON YYYY')  and disburse_mode = 'CH' AND d.disb_status not in ('X', 'R','S'))
+                                    or (trunc(d.autodebit_Date) <= to_Date(?, 'DD MON YYYY') and disburse_mode = 'AD' AND d.disb_status not in ('X', 'R','S') and (length(d.DEBIT_BANKACCT) > 0)))
+                                    and m.account_status <> 2
+                                    group by SUBSTR(UF_GET_PRODUCT_CATG(m.PRODUCT_CODE, m.PRODUCT_SUB_CODE), 1, 3)
+                                    union
+                                    SELECT count(*) AS bil,
+                                        count(distinct M.Nokpbaru) as bilpeminjam,
+                                        sum(M.Jumlahpinjaman) AS amt,
+                                        M.Produk_Category AS PRODUCT_CATG
+                                    FROM Migration_New M
+                                    group by M.Produk_Category
+                                )
+                                group by nvl(uf_decode_prodcatg(product_catg), 0)
+                            ", [$currentDate, $currentDate]);
 
-        $pivotData = [
-            'product_categories' => [],
-            'bil_peminjam' => [],
-            'bil_akaun' => [],
-            'jumlah_pembiayaan' => [],
-        ];
+            $pivotData = [
+                'product_categories' => [],
+                'bil_peminjam' => [],
+                'bil_akaun' => [],
+                'jumlah_pembiayaan' => [],
+            ];
 
-        foreach ($pembiayanData as $result) {
-            $pivotData['product_categories'][] = $result->product_category;
-            $pivotData['bil_peminjam'][] = $result->bil_peminjam;
-            $pivotData['bil_akaun'][] = $result->bilakaun;
-            $pivotData['jumlah_pembiayaan'][] = number_format($result->jumlah_pembiayaan, 2);
-        }
+            foreach ($pembiayanData as $result) {
+                $pivotData['product_categories'][] = $result->product_category;
+                $pivotData['bil_peminjam'][] = $result->bil_peminjam;
+                $pivotData['bil_akaun'][] = $result->bilakaun;
+                $pivotData['jumlah_pembiayaan'][] = number_format($result->jumlah_pembiayaan, 2);
+            }
 
-        $this->pembiayaan = $pivotData;
+            return $pivotData;
+        });
 
         $this->pmgiLevels = [
             'PM1' => 'PMG-i (1)',
@@ -124,7 +158,7 @@ class Pyd extends Component
     public function render()
     {
         $officerData = $this->getOfficerData('desc', 8);
-        $officerDataAsc = $this->getOfficerData('asc', 8);
+        $officerDataAsc = $officerData->reverse()->values()->toArray();
 
         return view('livewire.home.pyd', [
             'officerDatas' => $officerData,
@@ -134,11 +168,12 @@ class Pyd extends Component
 
     public function getOfficerData(string $order, int $limit)
     {
-        $officerId = auth()->user()->userid;
+        $officerId = $this->user;
 
         // Get summary data from PMGI_SUMM_MTH_OFFICER
         $summaryData = DB::table('PMGI_SUMM_MTH_OFFICER as a')
             ->where('a.officer_id', $officerId)
+            ->whereIn('a.incl_pmgi_flag', ['B', 'D'])
             ->select(
                 'a.report_date',
                 'a.bil_selia',
