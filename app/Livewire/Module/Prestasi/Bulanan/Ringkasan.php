445d255237e0173c7e695cb78a6f3d953b6066d4
Nazirul Fitri <nazirul@csc.net.my>
2024-11-06 15:46:18 +0800

    fix submonth

--- app/Livewire/Module/Prestasi/Bulanan/Ringkasan.php
+++ app/Livewire/Module/Prestasi/Bulanan/Ringkasan.php
@@ -50,7 +50,7 @@ class Ringkasan extends Component
     private function getAdminData(): Collection
     {
         $query = SummMthOfficer::with(['branch', 'officerBranch'])
-                                ->whereBetween('report_date', [$this->reportDate->copy()->subMonth()->startOfMonth(), $this->reportDate->copy()->endOfMonth()]);
+                                ->whereBetween('report_date', [$this->reportDate->copy()->subMonthNoOverflow()->startOfMonth(), $this->reportDate->copy()->endOfMonth()]);
 
         if ($this->state != '%') {
             $query->where('branch_state_code', $this->state);
@@ -80,7 +80,7 @@ class Ringkasan extends Component
         }
 
         return SummMthOfficer::whereAcctBranchCode($branch_code)
-            ->whereBetween('report_date', [$this->reportDate->copy()->subMonth()->startOfMonth(), $this->reportDate->copy()->endOfMonth()])
+            ->whereBetween('report_date', [$this->reportDate->copy()->subMonthNoOverflow()->startOfMonth(), $this->reportDate->copy()->endOfMonth()])
             ->orderBy('report_date', 'asc')
             ->orderBy('incl_pmgi_flag', 'asc') // Ensure branch totals (N) come after individual records
             ->get();
