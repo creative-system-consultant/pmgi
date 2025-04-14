df528892b52ffc711a7c72577d7826945624c39d
Nazirul Fitri <nazirul@csc.net.my>
2025-04-14 13:06:23 +0800

    style: reorder class attributes for consistency and readability
    This commit reorders the class attributes in the Blade template to maintain consistency and improve readability. The changes do not affect the functionality but ensure a uniform structure across the codebase.

--- resources/views/livewire/module/lantikan/evaluator/base-pmgi.blade.php
+++ resources/views/livewire/module/lantikan/evaluator/base-pmgi.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col p-4 my-4 mt-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6" x-cloak>
-    <div class="flex items-center justify-between">
+<div class="flex flex-col p-4 my-4 mt-4 bg-white rounded-lg border border-gray-200 shadow-sm sm:p-6" x-cloak>
+    <div class="flex justify-between items-center">
         <h3 class="text-xl font-bold text-gray-900">PMGi {{ $pmgiValue }}</h3>
 
         @if($datas->count() > 0)
@@ -11,7 +11,7 @@
         @endif
     </div>
 
-    <div class="mt-6 overflow-x-auto">
+    <div class="overflow-x-auto mt-6">
         <div class="inline-block min-w-full align-middle">
             <div class="overflow-hidden">
                 @if($datas->count() == 0)
@@ -36,26 +36,26 @@
                     <tbody class="bg-white">
                         @foreach ($datas as $index => $data)
                         <tr class=" @if($data->status == 1) bg-green-100 @endif">
-                            <td class="p-2 text-sm font-normal text-left text-gray-900 border border-black whitespace-nowrap">
+                            <td class="p-2 text-sm font-normal text-left text-gray-900 whitespace-nowrap border border-black">
                                 {{ $loop->iteration }}
                             </td>
-                            <td class="p-2 text-sm font-normal text-left text-gray-900 border border-black whitespace-nowrap">
+                            <td class="p-2 text-sm font-normal text-left text-gray-900 whitespace-nowrap border border-black">
                                 {{ $data->username }}
                             </td>
-                            <td class="p-2 text-sm font-normal text-left text-gray-900 border border-black whitespace-nowrap">
+                            <td class="p-2 text-sm font-normal text-left text-gray-900 whitespace-nowrap border border-black">
                                 {{ $data->branch_name }}
                             </td>
-                            <td class="p-2 text-sm font-normal text-left text-gray-900 border border-black whitespace-nowrap">
+                            <td class="p-2 text-sm font-normal text-left text-gray-900 whitespace-nowrap border border-black">
                                 {{ $data->jawatan }}
                             </td>
-                            <td class="p-1 text-sm font-normal text-center text-gray-500 border border-black whitespace-nowrap">
+                            <td class="p-1 text-sm font-normal text-center text-gray-500 whitespace-nowrap border border-black">
                                 <div class="flex justify-center">
                                     @if($data->status == 0)
                                         <x-checkbox id="checkbox-{{ $data->userid }}" value="{{ $data->userid }}" wire:model="selection" />
                                     @else
                                         <div class="text-gray-900">
                                             <span class="block">PYM: {{ $data->pym_name ?? 'N/A' }}</span>
-                                            @if($data->pmgi_level == '3')
+                                            @if($data->pmgi_level == 'PM3')
                                             <span class="block">PMC: {{ $data->pmc_name ?? 'N/A' }}</span>
                                             @endif
                                         </div>
@@ -75,7 +75,7 @@
     <x-modal.card title="Pilih PYM {{ ($pmgi == 3) ? 'dan PMC' : '' }} bagi PMGi {{ $pmgi }}" blur align="center" max-width="lg" wire:model="cardModal">
         <div>
             <x-select
-                class="flex-1 block w-24 mr-4 text-sm text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+                class="block flex-1 mr-4 w-24 text-sm text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500"
                 Label="Pegawai Yang Menilai (PYM)"
                 placeholder="Sila Pilih"
                 :options="$pym"
@@ -86,7 +86,7 @@
 
             @if($pmgi == 3)
                 <x-select
-                    class="flex-1 block w-24 mt-4 mr-4 text-sm text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+                    class="block flex-1 mt-4 mr-4 w-24 text-sm text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500"
                     Label="Pegawai Pemudah Cara (PMC)"
                     placeholder="Sila Pilih"
                     :options="$pmc"
@@ -98,7 +98,7 @@
         </div>
 
         <x-slot name="footer">
-            <div class="flex justify-between gap-x-4">
+            <div class="flex gap-x-4 justify-between">
                 <x-button flat label="Cancel" x-on:click="close" />
                 <x-button primary label="Save" wire:click="save" />
             </div>
