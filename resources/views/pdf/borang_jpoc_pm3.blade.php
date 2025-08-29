0703e3b8cdaea3aeacc50ea156bbb952612b695d
Hafiz-azhar1 <hafiz.azhar@csc.net.my>
2025-08-29 17:15:31 +0800

    gitflow-feature-stash: Add_ulasan_pdfJpoc

--- resources/views/pdf/borang_jpoc_pm3.blade.php
+++ resources/views/pdf/borang_jpoc_pm3.blade.php
@@ -198,7 +198,7 @@
             </div>
             
             <div class="input-container">
-                <img src="{{ $paths['image_path'] }}" alt="Generated Image" style="margin-top: 250px; width: 100%; object-fit: contain;">
+                <img src="{{ $paths['image'] }}" alt="Generated Image" style="margin-top: 250px; width: 100%; object-fit: contain;">
             </div>
         </div>
     </div>
@@ -233,15 +233,21 @@
     <div class="centered">
         <img src="image/borang/jpoc/BorangJpocPmgi12-4.jpg" alt="BORANG JPoc PMGi 12" style="margin-top: 200px" width="700" height="900">
         <div class="content-overlay">
-            {{-- ulasan --}}
-            {{-- <div class="input-container" style="top: 190px; left: 140px; width: 500px; word-wrap: break-word; white-space: normal;">
-                <strong>{{ $pymInfo->comments }}</strong>
-            </div> --}}
-
             {{-- pelan tindakan --}}
             <div class="input-container" style="top: 190px; left: 100px; width: 500px; word-wrap: break-word; white-space: normal;">
                 <strong>{{ $pymInfo->action }}</strong>
             </div>
+
+            {{-- kepala ulasan --}}
+             <div class="input-container" style="top: 300px; left: 90px; width: 500px; word-wrap: break-word; white-space: normal;">
+                <strong>Ulasan (Jika Ada) : </strong>
+            </div> 
+
+            {{-- ulasan --}}
+             <div class="input-container" style="top: 340px; left: 100px; width: 500px; word-wrap: break-word; white-space: normal;">
+                <strong>{{ $pymInfo->comments }}</strong>
+            </div> 
+
         </div>
     </div>
     <div class="page_break"></div>
